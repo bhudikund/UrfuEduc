c73e80af9415716a776d0940704bd28cbf6c3899
bhudikund <schooolmann@gmail.com>
2025-04-07 03:22:26 +0500

    upd

--- lab1/task1.cpp
+++ lab1/task1.cpp
@@ -1,23 +1,25 @@
-﻿//#include <iostream>
-//using namespace std;
-//
-//void main(void)
-//{
-//    int i, k;
-//    int power(int, int);
-//    for (i = 0; i < 10; i++)
-//    {
-//        k = power(2, i);
-//        cout << "i = " << i << ", k = " << k << endl;
-//    }
-//}
-//
-//int power(int x, int n)
-//{
-//    int i, p = 1;
-//    for (i = 1; i < n; i++)
-//    {
-//        p *= x;
-//    }
-//    return p;
-//}
+﻿#include <iostream>
+using namespace std;
+
+void main(void)
+{
+    int i, k;
+    int power(int, int);
+    // Вычисление  степеней от 0 до 9
+    for (i = 0; i < 10; i++)
+    {
+        k = power(2, i);
+        cout << "i = " << i << ", k = " << k << endl;
+    }
+}
+
+int power(int x, int n)
+{
+    int i, p = 1;
+    // Умножаем основание на себя n раз
+    for (i = 1; i < n; i++)
+    {
+        p *= x;
+    }
+    return p;
+}
--- lab1/task2.cpp
+++ lab1/task2.cpp
@@ -1,13 +1,13 @@
-//#define PR(x) printf("x=%u, *x=%d, &x=%u\n", x,*x,&x)
-//#include <stdio.h>
-//void main(void)
-//{
-//	int mas[] = { 100,200,300 };
-//	int *ptr1, *ptr2;
-//	ptr1 = mas;
-//	ptr2 = &mas[2];
-//	PR(ptr1);
-//	PR(ptr2);
-//	++ptr2;
-//	printf("ptr2-ptr1=%u\n", ptr2 - ptr1);
-//}
+#define PR(x) printf("x=%u, *x=%d, &x=%u\n", x,*x,&x)
+#include <stdio.h>
+void main(void)
+{
+	int mas[] = { 100,200,300 };
+	int *ptr1, *ptr2;
+	ptr1 = mas;
+	ptr2 = &mas[2];
+	PR(ptr1);
+	PR(ptr2);
+	++ptr2;
+	printf("ptr2-ptr1=%u\n", ptr2 - ptr1);
+}
